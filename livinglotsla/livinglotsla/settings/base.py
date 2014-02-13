96467830484d2f916468896f22b19316f0f972a0
Eric Brelsford <ebrelsford@gmail.com>
2014-02-13 11:35:11 -0500

    LADATA_PROCESSED_DATA_DIR

--- livinglotsla/livinglotsla/settings/base.py
+++ livinglotsla/livinglotsla/settings/base.py
@@ -288,6 +288,7 @@ LIVING_LOTS = {
 LOCAL_PROJECTION = 2229
 
 LADATA_PARCEL_VIEWER_URL = 'http://maps.assessor.lacounty.gov/mapping/rolldata.asp?ain='
+LADATA_PROCESSED_DATA_DIR = get_env_variable('LADATA_PROCESSED_DATA_DIR')
 LADATA_UA = ('Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 '
              '(KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36')
 
