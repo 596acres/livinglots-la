00cde9b8fe1e5f6689d6e1692c73fb3644af91a3
Eric Brelsford <ebrelsford@gmail.com>
2014-02-14 00:49:55 -0500

    Deployment scripts

---
+++ deploy/start_gunicorn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NAME="laopenacres"
+DJANGODIR=$HOME/webapps/laopenacres/livinglots-la/livinglotsla
+PORT=<PORT>
+USER=laopenacres
+GROUP=laopenacres
+NUM_WORKERS=3
+DJANGO_SETTINGS_MODULE=laopenacres.settings.production
+DJANGO_WSGI_MODULE=laopenacres.wsgi
+VIRTUALENV=laopenacres
+LOGDIR=$HOME/webapps/llnola_django/logs
+
+echo "Starting $NAME"
+
+# Activate the virtual environment
+source ~/bin/virtualenvwrapper.sh
+workon $VIRTUALENV
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+
+# Start Django Unicorn
+exec gunicorn \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --log-level=debug \
+  --bind=127.0.0.1:$PORT \
+  --log-file $LOGDIR/error.log --access-logfile $LOGDIR/access.log \
+  $DJANGO_WSGI_MODULE:application
