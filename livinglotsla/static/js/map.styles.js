0258e4bcfcbb6393569badf3c8a151fcad63264e
Eric Brelsford <ebrelsford@gmail.com>
2014-02-17 12:07:29 -0500

    Private tax default

--- livinglotsla/static/js/map.styles.js
+++ livinglotsla/static/js/map.styles.js
@@ -7,6 +7,7 @@ define([], function () {
     return {
         in_use: '#BAB974',
         private: '#D4A7D4',
+        private_tax_default: '#9A649E',
         public: '#A9756D',
         public_remnant: '#8C474D'
     };
