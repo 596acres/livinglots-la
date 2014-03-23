5c063d8397d31001bed45a8382483c8d5e388ab6
Eric Brelsford <ebrelsford@gmail.com>
2014-03-22 20:00:57 -0400

    Don't assume a ZoningDistrict for each Lot

--- livinglotsla/lots/synchronizers.py
+++ livinglotsla/lots/synchronizers.py
@@ -13,10 +13,15 @@ class LotZoningSynchronizer(synchronizers.Synchronizer):
     def sync(self, data_source):
         lots = self.find_lots(batch_size=data_source.batch_size)
         for lot in lots:
-            lot.zoning_district = ZoningDistrict.objects.get(
-                geometry__contains=lot.centroid,
-            )
-            lot.save()
+            try:
+                lot.zoning_district = ZoningDistrict.objects.get(
+                    geometry__contains=lot.centroid,
+                )
+                lot.save()
+            except ZoningDistrict.DoesNotExist:
+                print 'No ZoningDistrict found for lot %d' % lot.pk
+            except ZoningDistrict.MultipleObjectsReturned:
+                print 'Multiple ZoningDistricts found for lot %d' % lot.pk
 
 
 register(LotZoningSynchronizer)
