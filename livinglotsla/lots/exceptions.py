5517216eb6dda4616332b2ba80020b0a18b46232
Eric Brelsford <ebrelsford@gmail.com>
2014-04-15 13:17:05 -0400

    Move create_lot_for_parcels to general package

--- livinglotsla/lots/exceptions.py
+++
@@ -1,6 +0,0 @@
-class ParcelAlreadyInLot(Exception):
-    """
-    An attempt was made to add a parcel to a lot but the parcel is already
-    part of a lot.
-    """
-    pass
--- livinglotsla/lots/models.py
+++ livinglotsla/lots/models.py
@@ -14,64 +14,13 @@ from livinglots_lots.models import (BaseLot, BaseLotGroup, BaseLotLayer,
                                     BaseLotManager)
 
 from organize.models import Organizer
-from owners.models import Owner
-
-from .exceptions import ParcelAlreadyInLot
 
 
 ureg = UnitRegistry()
 
 
 class LotManager(BaseLotManager):
-
-    def create_lot_for_parcels(self, parcels, **lot_kwargs):
-        lots = []
-
-        # Check parcel validity
-        for parcel in parcels:
-            if parcel.lot_model.count():
-                raise ParcelAlreadyInLot()
-
-        # Create lots for each parcel
-        for parcel in parcels:
-            kwargs = {
-                'parcel': parcel,
-                'polygon': parcel.geom,
-                'centroid': parcel.geom.centroid,
-                'address_line1': parcel.street_address,
-                'name': parcel.street_address,
-                'postal_code': parcel.zip_code,
-                'city': parcel.city,
-                'state_province': parcel.state or 'CA',
-            }
-            kwargs.update(**lot_kwargs)
-
-            # Create or get owner for parcels
-            if parcel.owner_name:
-                (owner, created) = Owner.objects.get_or_create(
-                    parcel.owner_name,
-                    defaults={
-                        'owner_type': parcel.owner_type,
-                    }
-                )
-                kwargs['owner'] = owner
-
-            lot = Lot(**kwargs)
-            lot.save()
-            lots.append(lot)
-
-        # Multiple lots, create a lot group
-        if len(lots) > 1:
-            example_lot = lots[0]
-            kwargs = {
-                'address_line1': example_lot.address_line1,
-                'name': example_lot.name,
-            }
-            kwargs.update(**lot_kwargs)
-            lot = LotGroup(**kwargs)
-            lot.save()
-            lot.update(lots=lots)
-        return lot
+    pass
 
 
 class LotGroupLotMixin(models.Model):
