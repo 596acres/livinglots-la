f823c955198b3180a146928b5cad4182937fb327
Eric Brelsford <ebrelsford@gmail.com>
2014-05-06 17:43:52 -0400

    Spacing because why not

--- deploy/django/exportlots.sh
+++ deploy/django/exportlots.sh
@@ -2,7 +2,9 @@
 
 source $HOME/.bashrc
 source $HOME/.virtualenvs/laopenacres/bin/activate
+
 GEOJSON_FILE=$DOWNLOAD_DATA_DIR/laopenacres_lots_$(date +%Y%m%d).geojson
 SHP_FILE=$DOWNLOAD_DATA_DIR/laopenacres_lots_$(date +%Y%m%d).shp
+
 django-admin.py exportlots > $GEOJSON_FILE
 ogr2ogr -f "ESRI Shapefile" $SHP_FILE $GEOJSON_FILE
