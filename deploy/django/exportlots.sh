3ae71dc3f0ea54f1a5b21ae2c1dfee33310b39d4
Eric Brelsford <ebrelsford@gmail.com>
2014-05-06 17:59:37 -0400

    Change to directory rather than use as prefix

--- deploy/django/exportlots.sh
+++ deploy/django/exportlots.sh
@@ -3,10 +3,11 @@
 source $HOME/.bashrc
 source $HOME/.virtualenvs/laopenacres/bin/activate
 
-GEOJSON_FILE=$DOWNLOAD_DATA_DIR/laopenacres_lots_$(date +%Y%m%d).geojson
-SHP_FILE_PREFIX=$DOWNLOAD_DATA_DIR/laopenacres_lots_$(date +%Y%m%d)
-ZIP_FILE=$DOWNLOAD_DATA_DIR/laopenacres_lots_$(date +%Y%m%d).zip
+GEOJSON_FILE=laopenacres_lots_$(date +%Y%m%d).geojson
+SHP_FILE_PREFIX=laopenacres_lots_$(date +%Y%m%d)
+ZIP_FILE=laopenacres_lots_$(date +%Y%m%d).zip
 
+cd $DOWNLOAD_DATA_DIR
 django-admin.py exportlots > $GEOJSON_FILE
 ogr2ogr -f "ESRI Shapefile" ${SHP_FILE_PREFIX}.shp $GEOJSON_FILE
 zip $ZIP_FILE ${SHP_FILE_PREFIX}.{dbf,prj,shp,shx}
