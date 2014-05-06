c32cbe1c7ea30b698f2e90fe287291155021759e
Eric Brelsford <ebrelsford@gmail.com>
2014-05-06 17:58:41 -0400

    Zip it

--- deploy/django/exportlots.sh
+++ deploy/django/exportlots.sh
@@ -4,7 +4,9 @@ source $HOME/.bashrc
 source $HOME/.virtualenvs/laopenacres/bin/activate
 
 GEOJSON_FILE=$DOWNLOAD_DATA_DIR/laopenacres_lots_$(date +%Y%m%d).geojson
-SHP_FILE=$DOWNLOAD_DATA_DIR/laopenacres_lots_$(date +%Y%m%d).shp
+SHP_FILE_PREFIX=$DOWNLOAD_DATA_DIR/laopenacres_lots_$(date +%Y%m%d)
+ZIP_FILE=$DOWNLOAD_DATA_DIR/laopenacres_lots_$(date +%Y%m%d).zip
 
 django-admin.py exportlots > $GEOJSON_FILE
-ogr2ogr -f "ESRI Shapefile" $SHP_FILE $GEOJSON_FILE
+ogr2ogr -f "ESRI Shapefile" ${SHP_FILE_PREFIX}.shp $GEOJSON_FILE
+zip $ZIP_FILE ${SHP_FILE_PREFIX}.{dbf,prj,shp,shx}
