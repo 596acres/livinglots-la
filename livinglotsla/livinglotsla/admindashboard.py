8ce9768604995db040095df29d53e47bbc267c16
Eric Brelsford <ebrelsford@gmail.com>
2014-05-12 18:13:30 -0400

    Organize admin dashboard a bit

--- livinglotsla/livinglotsla/admindashboard.py
+++ livinglotsla/livinglotsla/admindashboard.py
@@ -12,24 +12,59 @@ class LivingLotsDashboard(Dashboard):
         self.children.append(modules.ModelList(
             title=_('Site Content'),
             models=(
-                'elephantblog.*',
                 'feincms.module.page.*',
+                'flatblocks.*',
+                'pathways.*',
             ),
         ))
 
-        self.children.append(modules.AppList(
-            title=_('Applications'),
-            exclude=(
-                'django.contrib.*',
-                'elephantblog.*',
-                'feincms.module.page.*',
-                'livinglots_usercontent.*',
+        self.children.append(modules.ModelList(
+            title=_('Moderation'),
+            models=(
+                'django_monitor.*',
+            ),
+        ))
+
+        self.children.append(modules.ModelList(
+            title=_('Lots'),
+            models=(
+                'lots.*',
             ),
         ))
 
         self.children.append(modules.ModelList(
             title=_('Lot Content'),
-            models=('livinglots_usercontent.*',),
+            models=(
+                'livinglots_usercontent.*',
+                'organize.*',
+                'livinglots_organize.*',
+                'owners.*',
+                'groundtruth.*',
+                'steward.*',
+                'friendlyowners.*',
+                'livinglots_lots.*',
+            ),
+        ))
+
+        self.children.append(modules.AppList(
+            title=_('Other Applications'),
+            exclude=(
+                'django.contrib.*',
+                'django_monitor.*',
+                'elephantblog.*',
+                'feincms.module.page.*',
+                'flatblocks.*',
+                'friendlyowners.*',
+                'groundtruth.*',
+                'livinglots_lots.*',
+                'livinglots_organize.*',
+                'livinglots_usercontent.*',
+                'lots.*',
+                'organize.*',
+                'owners.*',
+                'pathways.*',
+                'steward.*',
+            ),
         ))
 
         self.children.append(modules.AppList(
