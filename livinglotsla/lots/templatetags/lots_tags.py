dd2db04bc6b28058f187e4e9acb82da41c78f3f6
Eric Brelsford <ebrelsford@gmail.com>
2014-05-12 10:34:21 -0400

    Link to LA County Assessor data

--- livinglotsla/lots/templatetags/lots_tags.py
+++ livinglotsla/lots/templatetags/lots_tags.py
@@ -35,12 +35,17 @@ class GetVacantReasons(AsTag):
 
     def get_value(self, context, lot):
         reasons = []
+        building_outlines_url = 'http://egis3.lacounty.gov/dataportal/2011/04/28/countywide-building-outlines/'
+        local_roll_url = 'http://assessor.lacounty.gov/extranet/outsidesales/gisdata.aspx'
+
         for l in lot.lots:
             if l.friendly_owner:
                 reasons.append('The owner opted to add it to our map')
             if l.parcel.is_coded_vacant:
-                reasons.append('Its use code is vacant according to the LA '
-                               'County Assessor')
+                reasons.append("""
+                    Its use code is vacant according to the
+                    <a href="%s" target="_blank">LA County Assessor</a>
+                """ % local_roll_url)
             if l.parcel.sidelot_set.count() > 0:
                 reasons.append("It is in the city's sidelot data")
             if l.parcel.transmissionline_set.count() > 0:
@@ -48,17 +53,17 @@ class GetVacantReasons(AsTag):
             if l.parcel.weedabatement_set.count() > 0:
                 reasons.append('It is marked for weed abatement')
             if not Building.objects.filter(geom__overlaps=lot.polygon).exists():
-                url = 'http://egis3.lacounty.gov/dataportal/2011/04/28/countywide-building-outlines/'
-                reason = """
+                reasons.append("""
                     There are no buildings on it according to the
                     <a target="_blank" href="%s">county building outlines</a>
-                """ % url
-                reasons.append(reason)
+                """ % building_outlines_url)
 
             improvement_value = l.parcel.local_roll.improvement_value
             if not improvement_value or improvement_value is 0:
-                reasons.append('It has no improvement value according to the '
-                               'LA County Assessor')
+                reasons.append("""
+                    It has no improvement value according to the
+                    <a href="%s" target="_blank">LA County Assessor</a>
+                """ % local_roll_url)
         return reasons
 
 
